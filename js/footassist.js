bd60f1ce5ea96d53e2521a8b33c892e9784fd7ab
ShoTagomura <shotagomuralucia@gmail.com>
2021-04-18 17:53:10 +0900

    Ver 2.1.0
    CSS周り(Profileの内容とか、そしてProfile(Twitter)のスマフォに於ける横幅はみ出しの修正(リストのURLに折り返し適用)とか)

--- js/footassist.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-var fots = document.getElementById("fots02");
-
-window.onload = function() {
-    var dates = new Date();
-    var years = dates.getFullYear();
-    
-    fots.innerHTML = "(C)2013-"+years+" FPOTFK";
-};
